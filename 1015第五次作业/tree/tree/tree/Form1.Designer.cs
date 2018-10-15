bda5306d1facece76b75aa60885566fead9fb904
blaveer <1914481614@qq.com>
2018-10-16 00:47:56 +0800

    5

---
+++ 1015第五次作业/tree/tree/tree/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace tree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1095, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(1016, 80);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(136, 30);
+            this.hScrollBar1.TabIndex = 1;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(904, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "左树生长点位置";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1052, 135);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "30";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(957, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "右树的角度";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1052, 192);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 29);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "20";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(957, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "左树的角度";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(957, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "右树的长度";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(957, 305);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 29);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "左树的长度";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1052, 246);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 29);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "0.6";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1052, 299);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 29);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "0.7";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 600);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ 1015第五次作业/tree/tree/tree/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tree
+{
+    public partial class Form1 : Form
+    {
+        //NUM num = new NUM();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (graphics == null)
+            {
+                graphics = this.CreateGraphics();
+            }
+            drawCayleyTree(10, 300, 350, 100, -Math.PI / 2);
+        }
+        private Graphics graphics;
+        
+        void drawCayleyTree(int n,double x0,double y0,double leng,double th)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            drawLine(x0, y0, x1, y1);
+
+            drawCayleyTree(n - 1, x1,y1, NUM.per1 * leng, th + NUM.th1);
+            drawCayleyTree(n - 1, x1 - leng * Math.Cos(th) * NUM.k, y1 - leng * Math.Sin(th) * NUM.k, NUM.per2 * leng, th - NUM.th2);
+        }
+        void drawLine(double x0,double y0, double x1, double y1)
+        {
+            try
+            {
+                graphics.DrawLine(Pens.Blue, (int)x0, (int)y0, (int)x1, (int)y1);
+            }
+            catch (OverflowException)
+            {
+                Font myFont = new Font("宋体", 60, FontStyle.Bold);
+                Brush bush = new SolidBrush(Color.Red);//填充的颜色
+                graphics.DrawString("溢出了", myFont, bush, 150, 130);
+            }
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            NUM.k = hScrollBar1.Value*1.0/(hScrollBar1.Maximum -hScrollBar1.Minimum);
+           // NUM.k = 0.3;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //this.textBox1.Attributes.Add("onkeydown", "keyDown(" + this.Button1.ClientID + ");");
+            textBox1.Font = new Font("宋体", 14, textBox1.Font.Style);
+            if (textBox1.Text != "")
+            {
+                NUM.th1 = double.Parse(textBox1.Text) * Math.PI / 180; 
+
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Font = new Font("宋体", 14, textBox1.Font.Style);
+            if (textBox2.Text != "")
+            {
+                NUM.th2 = double.Parse(textBox2.Text) * Math.PI / 180;
+            }
+               
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox3.Font = new Font("宋体", 14, textBox1.Font.Style);
+            if (textBox3.Text != "")
+            {
+                NUM.per1 = double.Parse(textBox2.Text);
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            textBox4.Font = new Font("宋体", 14, textBox1.Font.Style);
+            if (textBox4.Text != "")
+            {
+                NUM.per2 = double.Parse(textBox2.Text);
+            }
+        }
+    }
+    class NUM
+    {
+        public static double th1 = 30 * Math.PI / 180;
+        public static double th2 = 20 * Math.PI / 180;
+        public static double per1 = 0.6;
+        public static double per2 = 0.7;
+        public static double k = 0.0;
+    }
+}
